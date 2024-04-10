8e4a766995ea4b2979882e8f56af184a56cf915d
Carlos Loaiza <carlosdamian05329@gmail.com>
2024-04-09 21:07:20 -0500

    feat: agrega main

--- main.go
+++ main.go
@@ -1,5 +1,55 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+	"worker-pool-server/pkg/dispatcher"
+	"worker-pool-server/pkg/job"
+)
+
+func RequestHandler(w http.ResponseWriter, r *http.Request, jobQueue chan job.Job) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+
+	delay, err := time.ParseDuration(r.FormValue("delay"))
+	if err != nil {
+		http.Error(w, "Invalid delay", http.StatusBadRequest)
+		return
+	}
+
+	number, err := strconv.Atoi(r.FormValue("value"))
+	if err != nil {
+		http.Error(w, "Invalid number", http.StatusBadRequest)
+		return
+	}
+
+	name := r.FormValue("name")
+	if name == "" {
+		http.Error(w, "Invalid name", http.StatusBadRequest)
+		return
+	}
+
+	job := job.Job{Name: name, Delay: delay, Number: number}
+
+	jobQueue <- job
+	w.WriteHeader(http.StatusCreated)
+}
+
 func main() {
-	
+	const maxWorkers = 4
+	const maxQueueSize = 20
+
+	jobQueue := make(chan job.Job, maxQueueSize)
+	dispatcher := dispatcher.NewDispatcher(jobQueue, maxWorkers)
+	dispatcher.Run()
+
+	// http://localhost:8000/fib
+	http.HandleFunc("/fib", func(w http.ResponseWriter, r *http.Request) {
+		RequestHandler(w, r, jobQueue)
+	})
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
--- pkg/dispatcher/dispatcher.go
+++ pkg/dispatcher/dispatcher.go
@@ -1,9 +1,38 @@
 package dispatcher
 
-import "worker-pool-server/pkg/job"
+import (
+	"worker-pool-server/pkg/job"
+	"worker-pool-server/pkg/worker"
+)
 
 type Dispatcher struct {
 	WorkerPool chan chan job.Job
 	MaxWorkers int
 	JobQueue   chan job.Job
+}
+
+func NewDispatcher(jobQueue chan job.Job, workers int) *Dispatcher {
+	return &Dispatcher{
+		JobQueue:   jobQueue,
+		MaxWorkers: workers,
+		WorkerPool: make(chan chan job.Job, workers),
+	}
+}
+
+func (d *Dispatcher) Dispatch() {
+	for {
+		job := <-d.JobQueue
+		go func() {
+			workerJobQueue := <-d.WorkerPool
+			workerJobQueue <- job
+		}()
+	}
+}
+
+func (d *Dispatcher) Run() {
+	for i := 0; i < d.MaxWorkers; i++ {
+		worker := worker.NewWorker(i, d.WorkerPool)
+		worker.Start()
+	}
+	go d.Dispatch()
 }
